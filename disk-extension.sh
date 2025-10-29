737648836d1d7239c69ee933f732f2f805f81abc
msaivenkatasiva <sivasl0597@gmail.com>
2025-10-30 00:06:41 +0530

    resizedisk

---
+++ disk-extension.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
