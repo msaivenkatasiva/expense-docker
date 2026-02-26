a5fd64fce9b8a962d7bc739154a9a896c55ea3de
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-26 09:20:02 +0530

    rds

--- backend/DbConfig.js
+++ backend/DbConfig.js
@@ -4,3 +4,10 @@ module.exports = Object.freeze({
     DB_PWD : 'ExpenseApp1',
     DB_DATABASE : 'transactions'
 });
+
+// module.exports = Object.freeze({
+//     DB_HOST : process.env.DB_HOST,
+//     DB_USER : process.env.DB_USER,
+//     DB_PWD : process.env.DB_PWD,
+//     DB_DATABASE : process.env.DB_DATABASE
+// });
